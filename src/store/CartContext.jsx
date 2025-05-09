d15fef8ea12603654eec2d553d20d8ee8f72c0b0
adnerramos21 <adnerramos21@gmail.com>
2025-05-09 12:21:16 -0500

    Created CartContext + context functionality

---
+++ src/store/CartContext.jsx
@@ -0,0 +1,86 @@
+import { createContext, useReducer } from "react";
+
+const CartContext = createContext({
+  items: [],
+  addItem: (item) => {},
+  removeItem: (id) => {},
+});
+
+function cartReducer(state, action) {
+  if (action.type === "ADD_ITEM") {
+    // state.items.push(action.item); // THIS IS NOT A GOOD PRACTICE
+    // push will update the existing state items array before the cart reducer is done executing
+    // it's recommended to verify its existence
+
+    const updatedItems = [...state.items];
+
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+
+    // This is how you update the state in an immutable way
+    if (existingCartItemIndex > -1) {
+      const existingItem = state.items[existingCartItemIndex];
+
+      const updatedItem = {
+        ...existingItem,
+        quantity: existingItem.quantity + 1,
+      };
+
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      // When no items were previously added
+      updatedItems.push({ ...action.item, quantity: 1 });
+    }
+
+    return { ...state, items: updatedItems };
+  }
+
+  if (action.type === "REMOVE_ITEM") {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+
+    const existingCartItem = state.items[existingCartItemIndex];
+
+    const updatedItems = [...state.items];
+
+    if (existingCartItem.quantity === 1) {
+      updatedItems.splice(existingCartItemIndex, 1);
+    } else {
+      const updatedItem = {
+        ...existingCartItem,
+        quantity: existingCartItem.quantity - 1,
+      };
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
+    return { ...state, items: updatedItems };
+  }
+
+  return state;
+}
+
+export function CartContextProvider({ children }) {
+  const [cart, dispatchCartAction] = useReducer(cartReducer, { items: [] });
+
+  function addItem(item) {
+    dispatchCartAction({ type: "ADD_ITEM", item });
+  }
+
+  function removeItem(id) {
+    dispatchCartAction({ type: "REMOVE_ITEM", id });
+  }
+
+  const cartContext = {
+    items: cart.items,
+    addItem,
+    removeItem,
+  };
+
+//   console.log(cartContext);
+
+  return <CartContext value={cartContext}>{children}</CartContext>;
+}
+
+export default CartContext;
